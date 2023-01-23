89b1c0ffc57f75a0f9f78e9ab6f7294f923f9a3f
Zero <Zero@hotmail.com>
2023-01-22 22:17:10 -0500

    ok

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,6 @@
 # paa hacer mi rutas , djangoframewok ya tiene rutas ya realizadas
+#resst_framework ya viene instalando todo el crud para eliminar. 
+#con router
 from rest_framework import routers
 from .api import ProjectViewSet 
 
